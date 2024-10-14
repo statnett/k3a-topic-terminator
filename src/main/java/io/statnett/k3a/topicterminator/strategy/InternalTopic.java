eba6e346dc4afd24d5c4b8c1b42c6db66b80d6eb
Erik Godding Boye <egboye@gmail.com>
2024-10-14 08:19:08 +0200

    internal just internal

--- src/main/java/io/statnett/k3a/topicterminator/strategy/InternalTopic.java
+++ src/main/java/io/statnett/k3a/topicterminator/strategy/InternalTopic.java
@@ -7,7 +7,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 /**
- * Topic that is internal, i.e., used by Kafka components, e.g. stream, connect, schema-registry
+ * Topic that is internal, i.e. used by Kafka components.
  */
 public class InternalTopic implements ReservedTopic {
     private final Set<String> allNames;
@@ -24,18 +24,6 @@ public class InternalTopic implements ReservedTopic {
     }
 
     private boolean isInternal(String name) {
-        return isUsedByConnect(name) || isUsedInternal(name) || isUsedByStream(name);
-    }
-
-    private boolean isUsedInternal(String name) {
         return name.startsWith("_");
-    }
-
-    private boolean isUsedByConnect(String name) {
-        return name.startsWith("stream.connect");
-    }
-
-    private boolean isUsedByStream(String name) {
-        return name.endsWith("-changelog") || name.endsWith("-repartition") || name.endsWith("-rekey");
     }
 }
