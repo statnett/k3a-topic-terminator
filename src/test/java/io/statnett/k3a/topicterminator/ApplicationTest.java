57fab3c577f108098529815452d265f73b616242
Sverre H. Huseby <sverre.huseby@statnett.no>
2025-11-24 16:12:05 +0100

    fix(deps): update spring boot to v4 (#111)

--- src/test/java/io/statnett/k3a/topicterminator/ApplicationTest.java
+++ src/test/java/io/statnett/k3a/topicterminator/ApplicationTest.java
@@ -10,7 +10,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.config.TopicConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
+import org.springframework.boot.micrometer.metrics.test.autoconfigure.AutoConfigureMetrics;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -27,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
-@AutoConfigureObservability
+@AutoConfigureMetrics
 // Using profile to disable scheduling for tests
 @ActiveProfiles("spring-boot-test")
 public class ApplicationTest {
